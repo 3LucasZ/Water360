6f17801330686c416a757eaa3098b1fc45101453
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-11 00:02:12 -0700

    Release testing v0.1.5

--- src/app/api/station/updSettings/route.ts
+++ src/app/api/station/updSettings/route.ts
@@ -5,12 +5,16 @@ export async function POST(request: NextRequest) {
   //get data
   const data = await request.json();
   //save settings
-  writeFileSync(settingsDir + "/IP.txt", data["IP"]);
-  global.IP = data["IP"];
-  writeFileSync(settingsDir + "/MAC.txt", data["MAC"]);
-  global.MAC = data["MAC"];
-  writeFileSync(settingsDir + "/RTMP.txt", data["RTMP"]);
-  global.RTMP = data["RTMP"];
+  try {
+    writeFileSync(settingsDir + "/IP.txt", data["IP"]);
+    global.IP = data["IP"];
+    writeFileSync(settingsDir + "/MAC.txt", data["MAC"]);
+    global.MAC = data["MAC"];
+    writeFileSync(settingsDir + "/RTMP.txt", data["RTMP"]);
+    global.RTMP = data["RTMP"];
+  } catch (e) {
+    return NextResponse.json({ err: e }, { status: 500 });
+  }
   //return
   return NextResponse.json({ msg: "ok" }, { status: 200 });
 }
