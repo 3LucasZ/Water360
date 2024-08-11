e48ac37f6df82b334d6a322d1235cc358e4c50e2
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-11 00:26:46 -0700

    Release testing v0.1.7

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,13 @@
-import { app } from "electron";
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const os = await import("node:os");
     const fs = await import("node:fs");
+    // const electron = await import("electron");
 
     console.log("initializing global variables...");
 
-    // const appDir = os.homedir() + "/Water360";
-    const appDir = app.getPath("appData");
+    const appDir = os.homedir() + "/Water360";
+    // const appDir = electron.app.getPath("appData");
     const downloadsDir = appDir;
     const settingsDir = appDir + "/user_settings";
 
