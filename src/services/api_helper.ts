d7856fd18e69719520549a35b184895d701d3472
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-17 01:07:10 -0700

    Update api_helper.ts

--- src/services/api_helper.ts
+++ src/services/api_helper.ts
@@ -1,11 +1,34 @@
 import { notifications } from "@mantine/notifications";
 import { NextResponse } from "next/server";
 
-export async function under360(route: string, body?: any) {
+export async function under360(
+  route: string,
+  body?: any,
+  disableNotification?: boolean
+) {
   const newRoute = "/under360" + route;
-  return await api(newRoute, body, route.includes("inspect"));
+  try {
+    return await api(newRoute, body, route.includes("inspect"));
+  } catch (e) {
+    if (!disableNotification) {
+      notifications.show({
+        title: "Error",
+        message: "an unknown error occured",
+        color: "red",
+      });
+    }
+    return NextResponse.json(
+      { err: "an unknown error occured" },
+      { status: 500 }
+    );
+  }
 }
-export async function api(route: string, body?: any, forceCache?: boolean) {
+export async function api(
+  route: string,
+  body?: any,
+  forceCache?: boolean,
+  disableNotification?: boolean
+) {
   const res = await fetch("/api" + route, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -14,7 +37,7 @@ export async function api(route: string, body?: any, forceCache?: boolean) {
   });
   //extract json in order to send notification if necessary
   const resJson = await res.json();
-  if (res.status != 200 && res.status != 504) {
+  if (res.status != 200 && res.status != 504 && !disableNotification) {
     notifications.show({
       title: "Error",
       message: "err" in resJson ? resJson["err"] : JSON.stringify(resJson),
