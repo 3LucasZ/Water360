57a4bdbf87dd87c72bc20734e41c0f3339abfe1a
3LucasZ <lucas.zheng@warriorlife.net>
2024-08-18 01:55:16 -0700

    more robust filehelper

--- src/services/file_helper.ts
+++ src/services/file_helper.ts
@@ -2,11 +2,15 @@ export function getFileName(filePath: string) {
   return filePath.substring(filePath.lastIndexOf("/") + 1);
 }
 export function getFilePrefix(filePath: string) {
-  return getFileName(filePath).substring(0, 3);
+  return getFileName(filePath).split("_")[0];
 }
 export function getFileSuffix(filePath: string) {
   return filePath.split(".").pop();
 }
 export function getFileStamp(filePath: string) {
-  return getFileName(filePath).substring(3);
+  return (
+    getFileName(filePath).split("_")[1] +
+    getFileName(filePath).split("_")[2] +
+    getFileName(filePath).split("_")[3]
+  );
 }
